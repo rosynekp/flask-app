774a0e08e80ea33bf0d581f73906a200f07829f3
rosynekp@msoe.edu <rosynekp@msoe.edu>
2024-02-05 07:02:56 -0600

    enhanced app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello MSOE'
+    image_path = 'img.jpg'
+    return send_file(image_path, mimetype='image/jpeg')
 
 if __name__ == "__main__":
     app.run()
